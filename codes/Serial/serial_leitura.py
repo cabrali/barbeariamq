84a819e2413cd66afb2a44ecc19c203647f4edb6
cabrali <147567915+cabrali@users.noreply.github.com>
2024-04-05 22:09:15 -0300

    Update and rename LICENSE to codes/Serial/serial_leitura.py
    serial_leitura.py

---
+++ codes/Serial/serial_leitura.py
@@ -0,0 +1,10 @@
+# -*- coding: iso-8859-1 -*-
+import serial
+
+# Abre porta Serial com seus devidos parâmetros
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=1)  
+
+# Lê repetidamente e imprime qualquer mensagem que vem do arduino
+while True:
+    msg = ser.readline().decode('ascii')
+    print(msg)
